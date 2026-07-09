488aa8b97a8508ad668ed7aed5a5700cb4166744
45000794Ndlakuse <ndlakusethamsanqa@gmail.com>
2026-07-09 21:21:07 +0200

    Populate phase files and Add navigation between application phase 1 and returning student

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -9,5 +9,29 @@ home_bp = Blueprint(
 
 @home_bp.route("/")
 def home():
-
     return render_template("index.html")
+
+
+@home_bp.route("/phase1")
+def phase1():
+    return render_template("phase1.html")
+
+
+@home_bp.route("/phase2")
+def phase2():
+    return render_template("phase2.html")
+
+
+@home_bp.route("/phase3")
+def phase3():
+    return render_template("phase3.html")
+
+
+@home_bp.route("/phase4")
+def phase4():
+    return render_template("phase4.html")
+
+
+@home_bp.route("/returning")
+def returning():
+    return render_template("returning.html")
